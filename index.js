86ab7124f2bc28ab1ec720617fa57a1054d88dd9
devedge <devedge@outlook.com>
2016-05-05 14:39:29 -0400

    Functional basic file code

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 
-// module.exports = {
-//     ReadFile: require('/home/usr/OtherDev/node-merkle-tree/lib/read-file.js')
-// }
+
+var merkle = require('./lib/user-methods.js');
 
 
+var args = {
+    file: '/home/usr/Desktop/here.mp4',
+    algo: 'sha256',
+    blocksize: 1048576
+}
 
+merkle.fromFile(args, function (err, tree) {
 
-// var _fastMap = new FastMap();
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('tree: ' + tree.root);
+        console.log('levels: ' + tree.numlevels);
+        console.log('leaves: ' + tree.numleaves);
+    }
+    
+});
 
-// console.log();
-
-
-
-
-// rf.hashBlocks('/home/usr/OtherDev/NodeJS_Local/blwanop.mp4', function (err, fastMap) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         this._fastMap = fastMap;
-//         console.log(_fastMap);
-//     }
-// });
-
-// console.log('yolo');
 
 
---
+++ somefunction.js
@@ -0,0 +1,8 @@
+
+ 
+var nmt = require('./lib/user-methods');
+
+var tree = nmt('/home/usr/Ritzer\ 1996,\ Abridged.pdf');
+
+console.log(tree);
+
