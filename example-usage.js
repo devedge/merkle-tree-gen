24358f78dcd69c1b9bfb2eef89580d4f96e7c8c9
devedge <devedge@outlook.com>
2016-05-05 17:51:41 -0400

    significant progress on tree generation

--- example-usage.js
+++ example-usage.js
@@ -1,34 +1,33 @@
-
-
+// Import the package
 var merkle = require('./index.js');
 
-
+// Specify the arguments for the tree
 var fileargs = {
-    file: '/home/usr/Desktop/here.mp4',
+    file: '/home/usr/largefile.mkv',
     hashalgo: 'sha256',
     blocksize: 1048576
 }
 
+// Async merkle generation. This can take some time depending
+// on the filesize.
 merkle.fromFile(fileargs, function (err, tree) {
 
     if (err) {
         console.log(err);
     } else {
-        console.log('tree: ' + tree.root);
-        console.log('levels: ' + tree.numlevels);
-        console.log('leaves: ' + tree.numleaves);
+        console.log(JSON.stringify(tree));
     }
     
 });
 
 
-var arrayargs = {
-    array: [],
-    hashalgo: 'sha256',
-    hashlist: true
-}
+// var arrayargs = {
+//     array: [],
+//     hashalgo: 'sha256',
+//     hashlist: true
+// }
 
 
-merkle.fromArray(arrayargs, function (err, tree) {
+// merkle.fromArray(arrayargs, function (err, tree) {
     
-});
+// });
--- index.js
+++ index.js
@@ -2,10 +2,35 @@
 
     'use strict';
 
+    /**
+     * fromArray()
+     *
+     * Generates a Merkle Tree from an array with the arguments passed in. The
+     * array cannot be empty. It can contain either values to be hashed, or an 
+     * actual list of hashes.
+     *
+     * @param {object} args An object containing the arguments to construct 
+     *                      the Tree. See the README for more info.
+     * @param {callback} cb An object containing the Merkle Tree. See 
+     *                      the README for more info.
+     *
+     */
     function fromArray(args, cb) {
         
     }
 
+    /**
+     * fromFile()
+     *
+     * Generates a Merkle Tree from the arguments passed into args. The absolute path 
+     * to the file must be specified.
+     *
+     * @param {object} args An object containing the arguments to construct 
+     *                      the Tree. See the README for more info.
+     * @param {callback} cb An object containing the Merkle Tree. See 
+     *                      the README for more info.
+     *
+     */
     function fromFile(args, cb) {
 
         var file;
@@ -31,11 +56,11 @@
         }
 
         // Import dependencies
-        var ReadFile = require('./lib/hash-file.js');
+        var HashFile = require('./lib/hash-file.js');
         var genMerkle = require('./lib/merkle-gen');
 
         // Initialize the blockreader
-        var blockreader = new ReadFile(hashalgo, blocksize);
+        var blockreader = new HashFile(hashalgo, blocksize);
 
         // Hash the file with the block size
         blockreader.hashBlocks(file, function (fastMap) {
@@ -47,6 +72,7 @@
         });
     }
 
+    // Export the fromFile() and fromArray() functions
     module.exports = {
         fromFile: fromFile,
         fromArray: fromArray
