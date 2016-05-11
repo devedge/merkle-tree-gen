44390e91c36a6a791433501c154f47ec30208479
devedge <devedge@outlook.com>
2016-05-10 22:44:04 -0400

    adding tests

---
+++ tests/test-hash-arrays.js
@@ -0,0 +1,37 @@
+
+// import merkle-tree-gen manually
+var merkle = require('../index');
+
+
+// even list
+var arraylist1 = [];
+
+// odd list
+var arraylist2 = [];
+
+// one-element list
+var arraylist3 = [];
+
+// empty list
+var arraylist4 = [];
+
+
+// pre-computed manually
+var expectedroot1 = '';
+var expectedroot2 = ''; 
+var expectedroot3 = ''; 
+
+
+
+
+
+
+/* Test on a valid even list, arraylist1 */
+
+/* Test on a valid odd list, arraylist2 */
+
+/* Test on a valid one-element list, arraylist3 */
+
+/* Test on an invalid empty list, arraylist4 */
+
+
