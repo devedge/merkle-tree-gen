71a711213edba324be02a22daeaa4213fc260e6a
devedge <devedge@outlook.com>
2016-09-04 00:05:26 -0400

    IFEEs are obsolete

--- tests/test-files.js
+++ tests/test-files.js
@@ -2,3 +2,15 @@
 var merkle = require('../index');
 var assert = require('assert');
 
+
+// first, hash a < 1 MiB file. 
+
+// then, hash a several MiB file
+
+// then, try to hash a file with a size of 0 MiB
+
+// hash a file that is evenly 3 MiB
+
+// hash a file just under 3 MiB
+
+// hash a file just over 3 MiB
